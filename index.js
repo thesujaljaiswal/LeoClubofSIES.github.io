c2858b7f293535ca46cd47c0ec572e465841bda7
anjaanid <sujal676865@ascs.sies.edu.in>
2023-04-18 22:55:29 +0530

    commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const element=document.getElementById("ham");
 console.log(element)
 document.getElementById("ham").addEventListener("click",()=>{
     console.log('click')
-    document.getElementById('hammenu').style.right=0;
+    // document.getElementById('hammenu').style.right=0;
 })
 document.getElementById("close").addEventListener("click",()=>{
     console.log('click')
-    document.getElementById('hammenu').style.right='-100%';
+    // document.getElementById('hammenu').style.right='-100%';
 })
